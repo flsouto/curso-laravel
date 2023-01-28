7b8212029710fd9556c9d38ad5a52cb1715d1a8b
flsouto <flsouto@gmail.com>
2023-01-28 02:40:21 -0300

    parte1

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+
+<div class="container mt-5">
+    <div class="row">
+        <form action="/login" method="POST" class="col-4 mx-auto">
+            
+            @csrf
+            <div class="form-group">
+                <label>Email:</label>
+                <input name="email" class="form-control" type="email" required />
+            </div>
+
+            <div class="form-group">
+                <label>Senha:</label>
+                <input name="senha" class="form-control" type="password" required />
+            </div>
+            <button class="btn btn-primary">Login</button>
+        </form>
+
+    </div>
+</div>
