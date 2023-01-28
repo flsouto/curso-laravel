f66d3ab4b44a6adb9dee4b8fb5f34455c0e38515
flsouto <flsouto@gmail.com>
2023-01-28 19:34:49 -0300

    parte2

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
-Route::get('/login',function(){
-    return view('login');
-});
+Route::get('/login',[LoginController::class, 'index']);
 
-Route::post('/login',function(){
-    dd("teste");
-});
+Route::post('/login',[LoginController::class, 'store']);
